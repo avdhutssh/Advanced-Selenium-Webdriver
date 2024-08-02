762a3ece13627e04758c74dabf05648cfb72f994
avdhutssh <avdhut.ssh@gmail.com>
2024-08-02 12:58:03 +0530

    Added TC suite for Key presses using Action class

--- src/test/java/com/herokuapp/theinternet/keypressestests/KeyPressesTests.java
+++ src/test/java/com/herokuapp/theinternet/keypressestests/KeyPressesTests.java
@@ -22,4 +22,16 @@ public class KeyPressesTests extends TestUtilities {
 		Assert.assertTrue(result.equals("You entered: ENTER"),
 				"result is not expected. \nShould be 'You entered: ENTER', but it is '" + result + "'");
 	}
+
+	@Test
+	public void pressKeyWithActionsTest() {
+		log.info("Starting pressKeyWithActionsTest");
+		WelcomePageObject welcomePage = new WelcomePageObject(driver, log);
+		welcomePage.openPage();
+		KeyPressesPage keyPressesPage = welcomePage.clickKeyPressesLink();
+		keyPressesPage.pressKeyWithActions(Keys.SPACE);
+		String result = keyPressesPage.getResultText();
+		Assert.assertTrue(result.equals("You entered: SPACE"),
+				"result is not expected. \nShould be 'You entered: ENTER', but it is '" + result + "'");
+	}
 }
