07cd8792ae056e8e2ece0588062885072a82dd61
avdhutssh <avdhut.ssh@gmail.com>
2024-08-02 12:54:32 +0530

    Added TC suite for Key presses using sendkeys with locator

---
+++ src/test/java/com/herokuapp/theinternet/keypressestests/KeyPressesTests.java
@@ -0,0 +1,25 @@
+package com.herokuapp.theinternet.keypressestests;
+
+import org.openqa.selenium.Keys;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.herokuapp.theinternet.base.TestUtilities;
+import com.herokuapp.theinternet.pages.EditorPage;
+import com.herokuapp.theinternet.pages.KeyPressesPage;
+import com.herokuapp.theinternet.pages.WelcomePageObject;
+
+public class KeyPressesTests extends TestUtilities {
+
+	@Test
+	public void pressKeyTest() {
+		log.info("Starting pressKeyTest");
+		WelcomePageObject welcomePage = new WelcomePageObject(driver, log);
+		welcomePage.openPage();
+		KeyPressesPage keyPressesPage = welcomePage.clickKeyPressesLink();
+		keyPressesPage.pressKey(Keys.ENTER);
+		String result = keyPressesPage.getResultText();
+		Assert.assertTrue(result.equals("You entered: ENTER"),
+				"result is not expected. \nShould be 'You entered: ENTER', but it is '" + result + "'");
+	}
+}
