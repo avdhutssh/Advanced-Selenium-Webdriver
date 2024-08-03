3bc6b022101c02445f2c6a9afa642e6426d1b46c
avdhutssh <avdhut.ssh@gmail.com>
2024-08-03 14:16:21 +0530

    added function for drag and drop using JS executor

--- src/main/java/com/herokuapp/theinternet/pages/BasePageObject.java
+++ src/main/java/com/herokuapp/theinternet/pages/BasePageObject.java
@@ -205,4 +205,27 @@ public class BasePageObject {
 		act.dragAndDrop(find(from), find(to)).perform();
 	}
 
+	/** Drag 'from' element to 'to' element using JS executor */
+	public void performDragAndDrop_Using_JS(By from, By to) {
+		JavascriptExecutor jsExecutor = (JavascriptExecutor) driver;
+		jsExecutor.executeScript(
+				"function createEvent(typeOfEvent) {\n" + "var event =document.createEvent(\"CustomEvent\");\n"
+						+ "event.initCustomEvent(typeOfEvent,true, true, null);\n" + "event.dataTransfer = {\n"
+						+ "data: {},\n" + "setData: function (key, value) {\n" + "this.data[key] = value;\n" + "},\n"
+						+ "getData: function (key) {\n" + "return this.data[key];\n" + "}\n" + "};\n"
+						+ "return event;\n" + "}\n" + "\n" + "function dispatchEvent(element, event,transferData) {\n"
+						+ "if (transferData !== undefined) {\n" + "event.dataTransfer = transferData;\n" + "}\n"
+						+ "if (element.dispatchEvent) {\n" + "element.dispatchEvent(event);\n"
+						+ "} else if (element.fireEvent) {\n" + "element.fireEvent(\"on\" + event.type, event);\n"
+						+ "}\n" + "}\n" + "\n" + "function simulateHTML5DragAndDrop(element, destination) {\n"
+						+ "var dragStartEvent =createEvent('dragstart');\n"
+						+ "dispatchEvent(element, dragStartEvent);\n" + "var dropEvent = createEvent('drop');\n"
+						+ "dispatchEvent(destination, dropEvent,dragStartEvent.dataTransfer);\n"
+						+ "var dragEndEvent = createEvent('dragend');\n"
+						+ "dispatchEvent(element, dragEndEvent,dropEvent.dataTransfer);\n" + "}\n" + "\n"
+						+ "var source = arguments[0];\n" + "var destination = arguments[1];\n"
+						+ "simulateHTML5DragAndDrop(source,destination);",
+				find(from), find(to));
+	}
+
 }
