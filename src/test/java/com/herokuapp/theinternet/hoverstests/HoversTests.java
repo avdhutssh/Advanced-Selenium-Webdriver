24acb17e23ee708d3850906dbc285cfaac2b50fb
avdhutssh <avdhut.ssh@gmail.com>
2024-08-03 14:50:55 +0530

    Added TC for Mouse hover

---
+++ src/test/java/com/herokuapp/theinternet/hoverstests/HoversTests.java
@@ -0,0 +1,24 @@
+package com.herokuapp.theinternet.hoverstests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.herokuapp.theinternet.base.TestUtilities;
+import com.herokuapp.theinternet.pages.HoversPage;
+import com.herokuapp.theinternet.pages.WelcomePage;
+
+public class HoversTests extends TestUtilities {
+
+	int userNumber = 2;
+
+	@Test
+	public void user2ProfileTest() {
+		log.info("Starting user2ProfileTest");
+		WelcomePage welcomePage = new WelcomePage(driver, log);
+		welcomePage.openPage();
+		HoversPage hoversPage = welcomePage.clickHoversLink();
+		hoversPage.openUserProfile(userNumber);
+		Assert.assertTrue(hoversPage.getCurrentUrl().contains("/users/" + userNumber),
+				"Url of opened page is not expected User 1 page url");
+	}
+}
