8a5cef16f426b688d2085bd2355a8652631dfdfa
avdhutssh <avdhut.ssh@gmail.com>
2024-08-04 20:53:02 +0530

    CSV_DP -> changes in test script

--- src/test/java/com/herokuapp/theinternet/loginpagetests/NegativeLogInTests.java
+++ src/test/java/com/herokuapp/theinternet/loginpagetests/NegativeLogInTests.java
@@ -1,35 +1,34 @@
 package com.herokuapp.theinternet.loginpagetests;
 
+import java.util.Map;
+
 import org.testng.Assert;
-import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
+import com.herokuapp.theinternet.base.CsvDataProviders;
 import com.herokuapp.theinternet.base.TestUtilities;
 import com.herokuapp.theinternet.pages.LoginPage;
 import com.herokuapp.theinternet.pages.WelcomePage;
 
 public class NegativeLogInTests extends TestUtilities {
 
-	@Parameters({ "username", "password", "expectedMessage" })
-	@Test(priority = 1)
-	public void negativeTest(String username, String password, String expectedErrorMessage) {
-		log.info("Starting negativeTest");
-
-		// open main page
+	@Test(dataProvider = "csvFileReader", dataProviderClass = CsvDataProviders.class )
+	public void negativeLogInTest(Map<String, String> testData) {
+		
+		String no = testData.get("no");
+		String username  = testData.get("username");
+		String password = testData.get("password");
+		String expectedErrorMessage = testData.get("expectedMessage");
+		String description = testData.get("description");
+		
+		log.info("Starting negativeLogInTest #" + no + " for " + description);
+		
 		WelcomePage welcomePage = new WelcomePage(driver, log);
 		welcomePage.openPage();
-
-		// Click on Form Authentication link
 		LoginPage loginPage = welcomePage.clickFormAuthenticationLink();
-
-		// execute negative login
 		loginPage.negativeLogIn(username, password);
-
-		// wait for error message
 		loginPage.waitForErrorMessage();
 		String message = loginPage.getErrorMessageText();
-
-		// Verification
 		Assert.assertTrue(message.contains(expectedErrorMessage), "Message doesn't contain expected text.");
 	}
 }
