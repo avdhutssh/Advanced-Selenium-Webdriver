1e2a6ce12730f0bfe45f5ac0cdf4280260cb6b8f
avdhutssh <avdhut.ssh@gmail.com>
2024-08-05 18:13:49 +0530

    Collecting JavaScript errors -> created function getBrowserLogs

--- src/main/java/com/herokuapp/theinternet/base/TestUtilities.java
+++ src/main/java/com/herokuapp/theinternet/base/TestUtilities.java
@@ -4,9 +4,13 @@ import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
+
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.logging.LogEntries;
+import org.openqa.selenium.logging.LogEntry;
 import org.testng.annotations.DataProvider;
 
 public class TestUtilities extends BaseTest {
@@ -43,5 +47,12 @@ public class TestUtilities extends BaseTest {
 
 	private static String getSystemTime() {
 		return (new SimpleDateFormat("HHmmssSSS").format(new Date()));
+	}
+
+	/** Get logs from browser console */
+	protected List<LogEntry> getBrowserLogs() {
+		LogEntries log = driver.manage().logs().get("browser");
+		List<LogEntry> logList = log.getAll();
+		return logList;
 	}
 }
