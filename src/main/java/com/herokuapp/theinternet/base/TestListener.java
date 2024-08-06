e35c66d744701c504c1a013e9d64fa2e9cfbc250
avdhutssh <avdhut.ssh@gmail.com>
2024-08-06 11:09:52 +0530

    TestNg Listeners-> creation of listener class

---
+++ src/main/java/com/herokuapp/theinternet/base/TestListener.java
@@ -0,0 +1,57 @@
+package com.herokuapp.theinternet.base;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+public class TestListener implements ITestListener {
+
+	Logger log;
+	String testName;
+	String testMethodName;
+
+	@Override
+	public void onTestStart(ITestResult result) {
+		this.testMethodName = result.getMethod().getMethodName();
+		log.info("[Starting " + testMethodName + "]");
+	}
+
+	@Override
+	public void onTestSuccess(ITestResult result) {
+		log.info("[Test " + testMethodName + " passed]");
+	}
+
+	@Override
+	public void onTestFailure(ITestResult result) {
+		log.info("[Test " + testMethodName + " failed]");
+		// TO-DO : on fail screenshot
+	}
+
+	@Override
+	public void onTestSkipped(ITestResult result) {
+		log.info("[Test " + testMethodName + " Skipped]");
+	}
+
+	@Override
+	public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
+	}
+
+	@Override
+	public void onTestFailedWithTimeout(ITestResult result) {
+	}
+
+	@Override
+	public void onStart(ITestContext context) {
+		this.testName = context.getCurrentXmlTest().getName();
+		this.log = LogManager.getLogger(testName);
+		log.info("[TEST " + testName + " STARTED]");
+	}
+
+	@Override
+	public void onFinish(ITestContext context) {
+		log.info("[ALL " + testName + " FINISHED]");
+	}
+
+}
