48127fc23e26f32cc220e7de7190345282047db2
avdhutssh <avdhut.ssh@gmail.com>
2024-08-08 19:19:17 +0530

    Mobile Emulation -> added parameter in setUp method

--- src/main/java/com/herokuapp/theinternet/base/BaseTest.java
+++ src/main/java/com/herokuapp/theinternet/base/BaseTest.java
@@ -21,24 +21,21 @@ public class BaseTest {
 	protected String testName;
 	protected String testMethodName;
 
-	@Parameters({ "browser", "chromeProfile" })
+	@Parameters({ "browser", "chromeProfile", "deviceName" })
 	@BeforeMethod(alwaysRun = true)
-	public void setUp(Method method, @Optional("chrome") String browser, @Optional String profile, ITestContext ctx) {
+	public void setUp(Method method, @Optional("chrome") String browser, @Optional String profile,
+			@Optional String deviceName, ITestContext ctx) {
 		log = LogManager.getLogger(testName);
 
 		BrowserDriverFactory factory = new BrowserDriverFactory(browser, log);
 		if (profile != null) {
 			driver = factory.createChromeWithProfile(profile);
+		} else if (deviceName != null) {
+			driver = factory.createChromeWithMobileEmulation(deviceName);
 		} else {
 			driver = factory.createDriver();
 		}
 
-		try {
-			Thread.sleep(3000);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-		
 		driver.manage().window().maximize();
 		this.testName = ctx.getCurrentXmlTest().getName();
 		this.testSuiteName = ctx.getSuite().getName();
